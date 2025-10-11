258611ba0d63f3d941451441dd8f49be8b1eaa27
Lina <dev@lina.moe>
2025-10-11 16:53:35 +0300

    fix: rename from old name

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 set -euo pipefail
 
 cd "$(dirname $(readlink -f $0))"
+[ ! -d build ] && mkdir build || true
 
 clang -c -o build/MyDelegate.o MyDelegate.m
 clang -c -o build/SettingsManager.o SettingsManager.m
 clang -c -o build/Keybind.o Keybind.m
-clang -c -o build/paster.o paster.m
+clang -c -o build/scratcher.o scratcher.m
 
-clang -o build/paster build/*.o -framework AppKit -framework Carbon
+clang -o build/scratcher build/*.o -framework AppKit -framework Carbon
